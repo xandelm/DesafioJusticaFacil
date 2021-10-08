6c0245f44a29fb1d82848476644972d73bb971c1
Alexandre Julio Lima Mendes <alexandrej.lima.mendes@gmail.com>
2021-10-08 18:30:27 -0300

    Adiciona arquivo .py. Esse script recebe uma entrada da linha de comando.

---
+++ desafio.py
@@ -0,0 +1,7 @@
+'''Recebe uma data de disponibilização de diarios oficiais
+do Supremo Tribunal Federal e retorna os MD5 dos diários 
+do dia recebido.'''
+
+import sys #possibilita entradas pela linha de comando.
+
+date = sys.argv[1:] #passa a entrada da linha de comando para date em forma de string
