45318d2fe75547b8ca731370c6a46d4856de9daf
Alexandre Julio Lima Mendes <alexandrej.lima.mendes@gmail.com>
2021-10-11 13:16:29 -0300

    Troca a data para dd/mm/aaaa. Adiciona 4 diarios para testes futuros.

--- desafio.py
+++ desafio.py
@@ -1,25 +1,26 @@
-'''Recebe uma data de disponibilização de diarios oficiais
+'''
+Recebe uma data de disponibilização de diarios oficiais
 do Supremo Tribunal Federal e retorna os MD5 dos diários 
-do dia recebido.'''
-
+do dia recebido.
+'''
 
 import sys #possibilita entradas pela linha de comando.
 import re #possibilita o uso de funcoes relacionadas a Regex
 
-DATE_LENGTH = 8
+DATE_LENGTH = 10
 datePatternRegex = re.compile(r'''(
 (\d{2})             # primeiros dois digitos(dia)
 (\/|-|\.|\s)        # separador
 (\d{2})             # dois digitos do meio(mes)
 (\/|-|\.|\s)        # separador
-(\d{2})             # ultimos dois digitos(ano)
+(\d{4})             # ultimos dois digitos(ano)
 )''',re.VERBOSE)
 
 
 date = sys.argv[1:] #passa a entrada da linha de comando para date(string)
 try:
     if len(date[0]) != DATE_LENGTH :
-        raise Exception('Data inválida. Digite a data no formato:00-00-00 ou 00/00/00')
+        raise Exception('Data inválida. Digite a data no formato:00-00-0000 ou 00/00/0000')
 except IndexError:
     print('Digite uma data ao rodar o script.')
     #propoe ao usuario a digitar a data no formato correto
@@ -27,5 +28,5 @@ match = re.search(datePatternRegex,date[0])
 if match:       #verifica se a data foi digitada no formato correto
     print('O usuario digitou a data no formato correto.')
 else:
-    raise Exception('Data inválida. Digite a data no formato:00-00-00 ou 00/00/00')
+    raise Exception('Data inválida. Digite a data no formato:00-00-0000 ou 00/00/0000')
     #propoe ao usuario a digitar a data no formato correto
