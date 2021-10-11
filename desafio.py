b8533da9165cfa3beafd5a2f6697c3795af3c9fc
Alexandre Julio Lima Mendes <alexandrej.lima.mendes@gmail.com>
2021-10-11 15:15:17 -0300

    Procura e imprime os diarios oficiais encontrados no mesmo diretorio de desafio.py

--- desafio.py
+++ desafio.py
@@ -6,6 +6,9 @@ do dia recebido.
 
 import sys #possibilita entradas pela linha de comando.
 import re #possibilita o uso de funcoes relacionadas a Regex
+import os
+from pathlib import Path
+import shutil
 
 DATE_LENGTH = 10
 datePatternRegex = re.compile(r'''(
@@ -17,16 +20,46 @@ datePatternRegex = re.compile(r'''(
 )''',re.VERBOSE)
 
 
-date = sys.argv[1:] #passa a entrada da linha de comando para date(string)
+diarioFileNameRegex = re.compile(r'(^DJE_)(\d+)_(\d+\.pdf$)')
+
+terminalInput = sys.argv[1:]
+date = terminalInput[0] #passa a entrada da linha de comando para date(string)
+
+print('date = '+date)
 try:
-    if len(date[0]) != DATE_LENGTH :
-        raise Exception('Data inválida. Digite a data no formato:00-00-0000 ou 00/00/0000')
+    if len(date) != DATE_LENGTH :
+        raise Exception('Data inválida. Digite a data no formato:dd-mm-aaaa ou dd/mm/aaaa')
 except IndexError:
     print('Digite uma data ao rodar o script.')
     #propoe ao usuario a digitar a data no formato correto
-match = re.search(datePatternRegex,date[0]) 
-if match:       #verifica se a data foi digitada no formato correto
-    print('O usuario digitou a data no formato correto.')
-else:
-    raise Exception('Data inválida. Digite a data no formato:00-00-0000 ou 00/00/0000')
-    #propoe ao usuario a digitar a data no formato correto
+match = re.search(datePatternRegex,date)
+if not match:
+    raise Exception('Data inválida. Digite a data no formato:dd-mm-aaaa ou dd/mm/aaaa')
+    #propoe ao usuario a rodar o scrip novamente digitando a data no formato correto
+
+ListaDiarios = []
+#for DiarioBaixado in Path.cwd().glob('DJE_*'):
+#    ListaDiarios.append(DiarioBaixado)
+#DiariosBaixados = list(Path.cwd().glob('DJE_*'))
+#for x in range(len(ListaDiarios)):
+#    print(ListaDiarios[x])
+
+
+
+for fileName in os.listdir(Path.cwd()): #Percorre os arquivos no diretório atual
+    matchObj = diarioFileNameRegex.search(fileName) #verifica se há algum diario oficial presente
+    if matchObj == None:
+        continue #se nao for um diario oficial, inicie uma nova interação do loop
+
+    ListaDiarios.append(fileName)
+
+for x in range(len(ListaDiarios)):
+    print(ListaDiarios[x])
+
+
+
+#os.listdir(Path.cwd()) will return a list of filename strings for each file in the path argument.
+#for filename in os.listdir(Path.cwd()):
+
+#p = Path('C:/Users/Al/Desktop')
+#list(p.glob('*.txt'))
